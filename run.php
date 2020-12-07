bd4ae532086eb5ee9b2ff9e8fcc9922212585703
Akim Krotov <akimsir@gmail.com>
2020-12-07 10:55:46 +0600

    самый главный файл забыл

---
+++ run.php
@@ -0,0 +1,21 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+if (!isset($argv[1])) {
+    die('Нужно указать команду');
+}
+
+if (!isset($argv[2])) {
+    die('Нужно указать файл с XML отчетом');
+}
+
+if ($argv[1] === 'getFailedTestCasesAsJson') {
+    $manager = new \JunitReports\XmlManager();
+
+    $failedTestCases = $manager->getFailedTestCasesAsArray($argv[2]);
+
+    echo json_encode($failedTestCases, JSON_UNESCAPED_UNICODE);
+}
+
+
+
